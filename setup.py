456073b6c500020c88baf43ffd2bdfef7caa8656
Vibhaj Rajan <vibhaj8@gmail.com>
2014-04-26 00:19:29 +0530

    starting off

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='django_rungevent',
+      version=version,
+      description="Django Gevent Run Command",
+      long_description="""\
+Django Gevent Run Command""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='django gevent',
+      author='Vibhaj Rajan',
+      author_email='vibhaj8@gmail.com',
+      url='https://github.com/tr4n2uil/django-rungevent',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
